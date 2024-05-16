4f05ddcc44bba270bcc2fd6903d12c793510cdc0
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-16 12:03:13 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Week_2_Challenge_Labs_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            makeTriangle();
+
+
+            static void makeTriangle() 
+            {
+                Console.WriteLine("Please enter a character");
+                char triangleCharacter = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Please enter a number");
+                int triangleWidth = Convert.ToInt32(Console.ReadLine());
+
+                for (int i = triangleWidth; i > 0; i--) 
+                {
+                    string line = new string(triangleCharacter, i);
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
